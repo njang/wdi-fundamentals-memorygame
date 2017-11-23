d8d122b55cfc3d204e017ceb88c968249fad7c7e
Neo <neo.w.jang@gmail.com>
2017-11-23 08:57:19 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,28 @@
-console.log("Up and running!");
+// console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0]; 
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[2]; 
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length == 2) { // if statement that checks to see if the length of the cardsInPlay array is 2.
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
